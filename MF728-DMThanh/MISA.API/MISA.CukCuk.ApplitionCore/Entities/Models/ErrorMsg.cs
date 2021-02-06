99ccedabfd40c335a8c13b8f9319d38c31976fb6
minhthanhhlht <minhthanhhlht@gmail.com>
2021-02-06 22:37:30 +0700

    Update Code MISA CukCuk Api

---
+++ MF728-DMThanh/MISA.API/MISA.CukCuk.ApplitionCore/Entities/Models/ErrorMsg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.ApplitionCore.Models
+{
+    public class ErrorMsg
+    {
+        /// <summary>
+        /// Thông báo lỗi
+        /// CreatedBy: DMThanh (01/02/2021)
+        /// </summary>
+        public string DevMsg { get; set; }
+        public string UserMsg { get; set; }
+        public string ErrorCode { get; set; }
+        public string MoreInfo { get; set; }
+        public string TraceId { get; set; }
+    }
+}
---
+++ MF728-DMThanh/MISA.API/MISA.CukCuk.ApplitionCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MISA.CukCuk.ApplitionCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
