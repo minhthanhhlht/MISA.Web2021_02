a8c954c408f27b6d1bb1f781d0aab87a79f79623
minhthanhhlht <minhthanhhlht@gmail.com>
2021-02-07 00:52:34 +0700

    Update UI

---
+++ MF728-DMThanh/MISA.API/MISA.CukCuk.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MISA.CukCuk.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+    }
+}
--- MF728-DMThanh/MISA.API/MISA.CukCuk.ApplitionCore/Entities/Models/ErrorMsg.cs
+++ MF728-DMThanh/MISA.API/MISA.CukCuk.ApplitionCore/Entities/Models/ErrorMsg.cs
@@ -11,10 +11,29 @@ namespace MISA.CukCuk.ApplitionCore.Models
         /// Thông báo lỗi
         /// CreatedBy: DMThanh (01/02/2021)
         /// </summary>
+
+        #region ErrorMsg
+        /// <summary>
+        /// Thông báo cho Dev
+        /// </summary>
         public string DevMsg { get; set; }
+        /// <summary>
+        /// Thông báo cho người dùng
+        /// </summary>
         public string UserMsg { get; set; }
+        /// <summary>
+        /// Mã lỗi
+        /// </summary>
         public string ErrorCode { get; set; }
+        /// <summary>
+        /// Thông tin thêm
+        /// </summary>
         public string MoreInfo { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
         public string TraceId { get; set; }
+        #endregion
     }
+    
 }
